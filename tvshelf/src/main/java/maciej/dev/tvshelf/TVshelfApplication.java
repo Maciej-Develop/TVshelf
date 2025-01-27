5bc625629fd02250794bc83187165fa6d9b153d3
Maciej Piechowski <58206@etu.he2b.be>
2025-01-27 14:41:11 +0100

    creation of the project

---
+++ tvshelf/src/main/java/maciej/dev/tvshelf/TVshelfApplication.java
@@ -0,0 +1,13 @@
+package maciej.dev.tvshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TVshelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TVshelfApplication.class, args);
+	}
+
+}
---
+++ tvshelf/src/test/java/maciej/dev/tvshelf/TVshelfApplicationTests.java
@@ -0,0 +1,13 @@
+package maciej.dev.tvshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TVshelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
